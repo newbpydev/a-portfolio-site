45b44c18039345f740f81b35b934e6a38c5cf796
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2023-08-11 22:08:51 -0300

    add images and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
+import AppLayout from "./components/layout/AppLayout";
+import Error from "./components/error/Error";
+import BlogPage from "./pages/BlogPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import AboutPage from "./pages/AboutPage";
+
+import "./styles/global.css";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage />,
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/blog",
+        element: <BlogPage />,
+      },
+      {
+        path: "/projects",
+        element: <ProjectsPage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,
+      },
+    ],
   },
 ]);
 
 function App() {
-  return <RouterProvider router={router}></RouterProvider>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
