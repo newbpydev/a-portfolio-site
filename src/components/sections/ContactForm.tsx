3da3c211db024f45ab6b6fc5ac6aa148423aea62
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2023-08-16 14:02:29 -0300

    chore(index.html): update document type declaration to use uppercase 'DOCTYPE' for HTML standard compliance chore(index.html): change page title to 'Juan's Profile' for better branding and personalization chore(ContactForm.tsx): add styling for contact form heading and form container for improved visual presentation chore(ContactForm.tsx): add background pattern to contact form for enhanced visual appeal chore(ContactForm.tsx): update form input styling for better alignment and spacing chore(ContactForm.tsx): increase font weight of body text in global CSS for improved readability

--- src/components/sections/ContactForm.tsx
+++ src/components/sections/ContactForm.tsx
@@ -4,17 +4,52 @@ import Button from "../buttons/Button";
 import { useState } from "react";
 import Textarea from "../form/Textarea";
 
-type Props = {};
-
 const StyledContactForm = styled.div`
   border-bottom: 1px solid white;
-
+  position: relative;
   padding-top: 6rem;
   padding-bottom: 9rem;
+
+  & .contact-heading {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    font-size: var(--text-sm);
+    line-height: 2.6rem;
+    margin-bottom: 5rem;
+
+    & h2 {
+      color: white;
+      font-size: 4rem;
+      line-height: 4rem;
+      font-weight: 700;
+      letter-spacing: -1.136px;
+      /* margin-bottom: 2rem; */
+    }
+  }
+
+  &::before {
+    position: absolute;
+    content: "";
+    bottom: 9.5rem;
+    left: -37rem;
+    width: 53rem;
+    height: 12.9rem;
+    background: url("images/patterns/pattern-rings.svg") no-repeat center
+      center/cover;
+  }
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+
+  gap: 3.2rem;
 `;
 
 //* COMPONENT: ContactForm
-export default function ContactForm({}: Props) {
+export default function ContactForm() {
   const [name, setName] = useState("");
   const [email, setemail] = useState("");
   const [message, setMessage] = useState("");
@@ -22,7 +57,7 @@ export default function ContactForm({}: Props) {
   // output
   return (
     <StyledContactForm>
-      <div>
+      <div className="contact-heading">
         <h2>Contact</h2>
         <p>
           I would love to hear about your project and how I could help. Please
@@ -30,7 +65,7 @@ export default function ContactForm({}: Props) {
         </p>
       </div>
 
-      <form>
+      <StyledForm>
         <Input
           placeholder="Name"
           onChange={(e) => setName(e.target.value)}
@@ -47,10 +82,11 @@ export default function ContactForm({}: Props) {
           placeholder="Message"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
+          rows={3}
         />
 
         <Button>Send Message</Button>
-      </form>
+      </StyledForm>
     </StyledContactForm>
   );
 }
